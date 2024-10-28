78b545060ce8f3c9fd6ef35c4ca747b34c473bc5
saman <saman.k8121@gmail.com>
2024-10-28 12:48:20 +0300

    chore: add sql script and change read me

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## Getting Started
+Create DB with executing product-db.sql in PostgreSql
+
 Add a `.env` file with the following keys:
 1. `CONNECTION_STRING` - Connection to the database
-2. `TOKEN_SECRET_KEY` - Secret key for JSON Web Token
+2. `TOKEN_SECRET_KEY` - Secret key for JSON Web Token - Execute this in your terminal: node -e "console.log(require('crypto').randomBytes(64).toString('hex'))"
 
 To start the development server, run:
 `npm run dev`
---
+++ product-db.sql
@@ -0,0 +1,317 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.4
+-- Dumped by pg_dump version 16.4
+
+-- Started on 2024-10-28 12:34:55
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 219 (class 1259 OID 16478)
+-- Name: cart; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.cart (
+    id integer NOT NULL,
+    "userId" integer NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
+    "updatedAt" timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.cart OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 16477)
+-- Name: cart_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.cart_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.cart_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4872 (class 0 OID 0)
+-- Dependencies: 218
+-- Name: cart_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.cart_id_seq OWNED BY public.cart.id;
+
+
+--
+-- TOC entry 224 (class 1259 OID 16561)
+-- Name: cart_product; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.cart_product (
+    id integer NOT NULL,
+    "productCount" integer DEFAULT 1 NOT NULL,
+    "cartId" integer NOT NULL,
+    "productId" integer NOT NULL
+);
+
+
+ALTER TABLE public.cart_product OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 16560)
+-- Name: cart_product_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.cart_product_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.cart_product_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4873 (class 0 OID 0)
+-- Dependencies: 223
+-- Name: cart_product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.cart_product_id_seq OWNED BY public.cart_product.id;
+
+
+--
+-- TOC entry 221 (class 1259 OID 16487)
+-- Name: product; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.product (
+    id integer NOT NULL,
+    caption character varying NOT NULL,
+    price numeric NOT NULL,
+    slug character varying NOT NULL,
+    weight character varying NOT NULL,
+    rate integer NOT NULL,
+    description character varying NOT NULL,
+    "imageSrc" character varying NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
+    "updatedAt" timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.product OWNER TO postgres;
+
+--
+-- TOC entry 220 (class 1259 OID 16486)
+-- Name: product_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.product_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.product_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4874 (class 0 OID 0)
+-- Dependencies: 220
+-- Name: product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.product_id_seq OWNED BY public.product.id;
+
+
+--
+-- TOC entry 217 (class 1259 OID 16463)
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."user" (
+    id integer NOT NULL,
+    username character varying NOT NULL,
+    email character varying NOT NULL,
+    password character varying NOT NULL,
+    "cartId" character varying,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
+    "updatedAt" timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public."user" OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 16462)
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.user_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4875 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
+
+
+--
+-- TOC entry 4694 (class 2604 OID 16481)
+-- Name: cart id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cart ALTER COLUMN id SET DEFAULT nextval('public.cart_id_seq'::regclass);
+
+
+--
+-- TOC entry 4700 (class 2604 OID 16564)
+-- Name: cart_product id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cart_product ALTER COLUMN id SET DEFAULT nextval('public.cart_product_id_seq'::regclass);
+
+
+--
+-- TOC entry 4697 (class 2604 OID 16490)
+-- Name: product id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product ALTER COLUMN id SET DEFAULT nextval('public.product_id_seq'::regclass);
+
+
+--
+-- TOC entry 4691 (class 2604 OID 16466)
+-- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+
+--
+-- TOC entry 4864 (class 0 OID 16487)
+-- Dependencies: 221
+-- Data for Name: product; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.product (id, caption, price, slug, weight, rate, description, "imageSrc", "createdAt", "updatedAt") FROM stdin;
+1	Milk	150	milk	100 ml	3	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar lorem massa, quis commodo dui volutpat id. Nullam accumsan sodales nibh eu porttitor. Pellentesque lorem sem, faucibus gravida tempus sed, hendrerit ut tellus. Integer tincidunt sem vel volutpat dictum. Duis enim nulla, vulputate id sem ac, volutpat bibendum justo. Vivamus augue felis, viverra sed vestibulum in, accumsan vitae lectus. Ut quis neque a nunc faucibus dictum. Pellentesque sit amet interdum lectus. Nullam vulputate tincidunt est at semper. Vestibulum non pellentesque tortor, nec sollicitudin lectus. Proin gravida turpis a justo feugiat, a venenatis metus hendrerit.	/images/products/milk.jpg	2024-09-06 12:47:32.46343	2024-09-06 12:47:32.46343
+2	Cola	200	cola	1.5 l	4	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar lorem massa, quis commodo dui volutpat id. Nullam accumsan sodales nibh eu porttitor. Pellentesque lorem sem, faucibus gravida tempus sed, hendrerit ut tellus. Integer tincidunt sem vel volutpat dictum. Duis enim nulla, vulputate id sem ac, volutpat bibendum justo. Vivamus augue felis, viverra sed vestibulum in, accumsan vitae lectus. Ut quis neque a nunc faucibus dictum. Pellentesque sit amet interdum lectus. Nullam vulputate tincidunt est at semper. Vestibulum non pellentesque tortor, nec sollicitudin lectus. Proin gravida turpis a justo feugiat, a venenatis metus hendrerit.	/images/products/cola.jpg	2024-09-06 12:47:32.46343	2024-09-06 12:47:32.46343
+3	Cheese	50	cheese	250 gr	2	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar lorem massa, quis commodo dui volutpat id. Nullam accumsan sodales nibh eu porttitor. Pellentesque lorem sem, faucibus gravida tempus sed, hendrerit ut tellus. Integer tincidunt sem vel volutpat dictum. Duis enim nulla, vulputate id sem ac, volutpat bibendum justo. Vivamus augue felis, viverra sed vestibulum in, accumsan vitae lectus. Ut quis neque a nunc faucibus dictum. Pellentesque sit amet interdum lectus. Nullam vulputate tincidunt est at semper. Vestibulum non pellentesque tortor, nec sollicitudin lectus. Proin gravida turpis a justo feugiat, a venenatis metus hendrerit.	/images/products/cheese.jpg	2024-09-06 12:47:32.46343	2024-09-06 12:47:32.46343
+4	Chips	0	chips	275 gr	4	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar lorem massa, quis commodo dui volutpat id. Nullam accumsan sodales nibh eu porttitor. Pellentesque lorem sem, faucibus gravida tempus sed, hendrerit ut tellus. Integer tincidunt sem vel volutpat dictum. Duis enim nulla, vulputate id sem ac, volutpat bibendum justo. Vivamus augue felis, viverra sed vestibulum in, accumsan vitae lectus. Ut quis neque a nunc faucibus dictum. Pellentesque sit amet interdum lectus. Nullam vulputate tincidunt est at semper. Vestibulum non pellentesque tortor, nec sollicitudin lectus. Proin gravida turpis a justo feugiat, a venenatis metus hendrerit.	/images/products/chips.jpg	2024-09-06 12:47:32.46343	2024-09-06 12:47:32.46343
+5	Butter	100	butter	100 gr	5	Butter Des	/images/products/butter.jpg	2024-09-06 12:47:32.46343	2024-09-06 12:47:32.46343
+\.
+
+
+--
+-- TOC entry 4711 (class 2606 OID 16496)
+-- Name: product PK_bebc9158e480b949565b4dc7a82; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product
+    ADD CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4709 (class 2606 OID 16485)
+-- Name: cart PK_c524ec48751b9b5bcfbf6e59be7; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cart
+    ADD CONSTRAINT "PK_c524ec48751b9b5bcfbf6e59be7" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4703 (class 2606 OID 16472)
+-- Name: user PK_cace4a159ff9f2512dd42373760; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4713 (class 2606 OID 16567)
+-- Name: cart_product PK_dccd1ec2d6f5644a69adf163bc1; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cart_product
+    ADD CONSTRAINT "PK_dccd1ec2d6f5644a69adf163bc1" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4705 (class 2606 OID 16474)
+-- Name: user UQ_78a916df40e02a9deb1c4b75edb; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE (username);
+
+
+--
+-- TOC entry 4707 (class 2606 OID 16476)
+-- Name: user UQ_e12875dfb3b1d92d7d7c5377e22; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE (email);
+
+
+--
+-- TOC entry 4714 (class 2606 OID 16578)
+-- Name: cart_product FK_139f8024067696fe5a8400ebda2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cart_product
+    ADD CONSTRAINT "FK_139f8024067696fe5a8400ebda2" FOREIGN KEY ("cartId") REFERENCES public.cart(id);
+
+
+--
+-- TOC entry 4715 (class 2606 OID 16583)
+-- Name: cart_product FK_4f1b0c66f4e0b4610e14ca42e5c; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cart_product
+    ADD CONSTRAINT "FK_4f1b0c66f4e0b4610e14ca42e5c" FOREIGN KEY ("productId") REFERENCES public.product(id);
+
+
+-- Completed on 2024-10-28 12:34:56
+
+--
+-- PostgreSQL database dump complete
+--
+
